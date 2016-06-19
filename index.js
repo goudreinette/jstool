e4d8d2c4fba45941259c7349f90a6b6ed224ed01
Rein van der Woerd <rwoerd2033@student.landstede.nl>
2016-06-19 19:22:12 +0200

    Added comments

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 #!/usr/bin/env node
+/*
+|--------------------------------------------------------------------------
+| JSTool
+|--------------------------------------------------------------------------
+| Copies over NPM scripts from ~/.scripts.json to package.json,
+| ignoring any locally defined scripts.
+|
+*/
 const program = require('commander')
 const sync    = require('./sync')
 
